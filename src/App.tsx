9bd420db4829499344d8fc4e35bd62214ca7f422
sungaee <sungaee121@naver.com>
2025-11-28 14:04:25 +0900

    🎨 Tailwind Css 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
-   <div>Hello World</div>
-  )
+    <div className="bg-black text-2xl font-bold underline">Hello World</div>
+  );
 }
 
-export default App
+export default App;
