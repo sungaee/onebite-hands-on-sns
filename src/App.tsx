914c73d7c343ae47b0dc9850b76aa68adad0a2fd
sungaee <sungaee121@naver.com>
2025-12-02 15:32:33 +0900

    🎨 Tailwind Css 백그라운드컬러, 사이즈, 여백

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,17 @@ function App() {
       <div className="text-xl font-extrabold">text-xl</div>
       <div className="text-2xl font-black">text-2xl</div>
       <div className="text-[23px]">text-23px</div>
+
+      {/* 2. 백그라운드컬러 */}
+      <div className="bg-amber-500">amber-500</div>
+
+      {/* 3. 사이즈 */}
+      <div className="h-20 w-full bg-blue-500">box</div>
+
+      {/* 4. 여백 */}
+      <div className="m-5 h-50 w-50 bg-red-400 px-5 py-5">
+        <div className="h-full w-full bg-blue-400"></div>
+      </div>
     </div>
   );
 }
