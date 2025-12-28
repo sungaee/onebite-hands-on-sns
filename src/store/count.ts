34cb670cce01cc784d6c32d06c32439f266d90a1
sungaee <sungaee121@naver.com>
2025-12-28 17:54:51 +0900

    🎨 combine , immer 미들웨어

--- src/store/count.ts
+++ src/store/count.ts
@@ -1,29 +1,41 @@
-import { Store } from "lucide-react";
 import { create } from "zustand";
+import { combine } from "zustand/middleware";
+import { immer } from "zustand/middleware/immer";
 
-type Store = {
-  count: number;
-  actions: {
-    increase: () => void;
-    decrease: () => void;
-  };
-};
+export const useCountStore = create(
+  immer(
+    combine({ count: 0 }, (set, get) => ({
+      actions: {
+        increase: () => {
+          set((state) => {
+            state.count += 1;
+          });
+        },
+        decrease: () => {
+          set((state) => {
+            state.count -= 1;
+          });
+        },
+      },
+    })),
+  ),
+);
 
-export const useCountStore = create<Store>((set, get) => ({
-  count: 0,
-  actions: {
-    increase: () => {
-      set((store) => ({
-        count: store.count + 1,
-      }));
-    },
-    decrease: () => {
-      set((store) => ({
-        count: store.count - 1,
-      }));
-    },
-  },
-}));
+// export const useCountStore = create<Store>((set, get) => ({
+//   count: 0,
+//   actions: {
+//     increase: () => {
+//       set((store) => ({
+//         count: store.count + 1,
+//       }));
+//     },
+//     decrease: () => {
+//       set((store) => ({
+//         count: store.count - 1,
+//       }));
+//     },
+//   },
+// }));
 
 export const useCount = () => {
   const count = useCountStore((store) => store.count);
