ff52b6a4859ec9c80146abcfb24c07daec7ba502
sungaee <sungaee121@naver.com>
2026-01-09 16:54:32 +0900

    ⚡️ Tanstack Query 서버설정 및 데이터조회 요청관리, 커스텀훅 분리

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,6 +9,11 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    watch: {
+      ignored: ["**/server/**"],
     },
   },
 });
