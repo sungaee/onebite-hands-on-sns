9270b6fc10cf5b590b80c5f6287d43c417d85ff9
sungaee <sungaee121@naver.com>
2026-01-16 14:29:57 +0900

    ⚡️ 캐시 데이터 다루기1-캐시 무효화 하기

--- src/hooks/queries/use-todo-data-by-id.ts
+++ src/hooks/queries/use-todo-data-by-id.ts
@@ -1,10 +1,11 @@
 import { fetchTodoById } from "@/api/fetch-todo-by-id";
+import { QUERY_KEYS } from "@/lib/constants";
 import { useQuery } from "@tanstack/react-query";
 
 export function useTodoDataById(id: string) {
   return useQuery({
     queryFn: () => fetchTodoById(id),
-    queryKey: ["todos", id],
+    queryKey: QUERY_KEYS.todo.detail(id),
     
     staleTime: 300000, // 데이터가 frash 상태일때만 유효
     gcTime: 5000, // 데이터가 inactive 일때만 동작
