096451dcf18b78771a439d6dd43c36611153fa4f
sungaee <sungaee121@naver.com>
2026-01-19 22:42:11 +0900

    ⚡️ 캐시 데이터 다루기3-낙관적 업데이트(2)

--- src/hooks/mutations/use-update-todo-mutaition.ts
+++ src/hooks/mutations/use-update-todo-mutaition.ts
@@ -5,10 +5,15 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useUpdateTodoMutation() {
   const queryClient = useQueryClient();
-  
+
   return useMutation({
     mutationFn: updateTodo,
-    onMutate: (updatedTodo) => {
+    onMutate: async (updatedTodo) => {
+      await queryClient.cancelQueries({
+        queryKey: QUERY_KEYS.todo.list,
+      });
+
+      const prevTodos = queryClient.getQueryData<Todo[]>(QUERY_KEYS.todo.list);
       queryClient.setQueryData<Todo[]>(QUERY_KEYS.todo.list, (prevTodos) => {
         if (!prevTodos) return [];
         return prevTodos.map((prevTodo) =>
@@ -16,6 +21,23 @@ export function useUpdateTodoMutation() {
             ? { ...prevTodo, ...updatedTodo }
             : prevTodo,
         );
+      });
+
+      return {
+        prevTodos,
+      };
+    },
+    onError: (error, variable, context) => {
+      if (context && context.prevTodos) {
+        queryClient.setQueryData<Todo[]>(
+          QUERY_KEYS.todo.list,
+          context.prevTodos,
+        );
+      }
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({
+        queryKey: QUERY_KEYS.todo.list,
       });
     },
   });
